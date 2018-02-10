ce3009fbb314925f727cb0bc3d79b220f10cd9b1
bethbenjamin <bethbenjamin@gmail.com>
2018-02-10 14:03:33 -0500

    working on program

--- VPShelter/Program.cs
+++ VPShelter/Program.cs
@@ -17,7 +17,18 @@ namespace VPShelter
              volunteer or manager play with individual pet
              manager to coordinate adoption of a pet which should display 
              a list of pet names and descriptions allowing a user to selct one*/
-            
+           
+            //objects of VirtualPet (Name, What they eat)
+            VirtualPet pet = new VirtualPet();
+            VirtualPet petOne = new VirtualPet("Draco", "Fwoopers");
+            VirtualPet petTwo = new VirtualPet("Thunderbird", "Pygmy Puffs");
+            //base class employee
+            //objects of employee are 1 manager and 2 volunteers
+
+            Manager managerOne = new Manager("Rubeus Hagrid");
+            Volunteer volOne = new Volunteer("Newton Scamander");
+            Volunteer volTwo = new Volunteer("Jacob Kowalski");
+
             
 
         }//end main
